d6846fc85a63b617b7407d10628475c2d00a14dc
cmylucas <155979046+cmylucas@users.noreply.github.com>
2024-03-04 10:45:45 -0500

    Update q1manchester.py

--- q1manchester.py
+++ q1manchester.py
@@ -2,12 +2,13 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 
 data = pd.read_csv('manchesterdata.csv')
 
 data = data.dropna()
-X = data[['Year', 'Median Sales Price', 'Total Population', 'Median Income Level']]
+X = data[['Median Sales Price', 'Total Population', 'Median Income Level']]
 y = data['Total Housing Units']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
@@ -26,7 +27,8 @@ intercept = model.intercept_
 mse = mean_squared_error(y_test, predictions)
 r2 = r2_score(y_test, predictions)
 
-formula = f"Total Housing Units = {intercept:.2f} + {coefficients[0]:.2f}*Year + {coefficients[1]:.2f}*Median Sales Price + {coefficients[2]:.2f}*Total Population + + {coefficients[3]:.2f}*Median Income Level"
+#formula = f"Total Housing Units = {intercept:.2f} + {coefficients[0]:.2f}*Year + {coefficients[1]:.2f}*Median Sales Price + {coefficients[2]:.2f}*Total Population + {coefficients[3]:.2f}*Median Income Level"
+formula = f"Total Housing Units = {intercept:.2f} + {coefficients[0]:.2f}*Median Sales Price + {coefficients[1]:.2f}*Total Population + {coefficients[2]:.2f}*Median Income Level"
 print("Linear Regression Formula:")
 print(formula)
 

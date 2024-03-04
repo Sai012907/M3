ca5680c4ff76c999d8f6f9e31e746e377cfd5778
Sailalitha Kodukula <66754230+Sai012907@users.noreply.github.com>
2024-03-04 09:13:39 -0500

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler
+
+data = pd.read_csv('dataset.csv')
+
+X = data.drop('target_var', axis = 1)
+y = data['target_var']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
